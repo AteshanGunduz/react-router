c62ce059c2d3045c147875dda14c1ea193b986ca
AteshanGunduz <atshn.gunduz@gmail.com>
2023-12-01 11:11:30 -1100

    react-router

---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import { Outlet } from 'react-router-dom'
+import Navbar from '../components/Navbar';
+
+
+
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <section className='section'>
+      <Outlet />
+      </section>
+    </>
+  );
+};
+
+export default Home;
